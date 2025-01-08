43a58e407fc4dbe09d802f3446673959664dc8fb
Rafael <138421856+JavaRaf@users.noreply.github.com>
2025-01-08 02:58:19 -0300

    first commit

--- fb/README.md
+++
@@ -1,2 +0,0 @@
-# FB Directory
-- This is where all the iterators, subtitle file and logs stored.
--- secret.sh
+++
@@ -1,6 +0,0 @@
-#!/bin/bash
-#
-# * This is where all the credentials is stored, this is used when theres no token supplied on actions.
-
-FRMENV_FBTOKEN=""
-FRMENV_GIFTOKEN=""
